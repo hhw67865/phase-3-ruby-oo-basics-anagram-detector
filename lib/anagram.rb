4846ee00570edbe3ad8893037ef51fb57d8106e7
Henry Wu <hhw67865@gmail.com>
2022-11-22 09:26:27 -0500

    commit tree 917dec068be3bbf3835b4ca59cf10fc1e0bafb63

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+    
+    def initialize (word)
+       @word = word.chars 
+    end
+
+    def match (array_of_strings)
+        i=0
+        match_array = []
+        while i<array_of_strings.length
+            letter_array = array_of_strings[i].chars
+            if @word.sort == letter_array.sort
+                match_array<<array_of_strings[i]
+            end
+            i+=1
+        end
+        match_array
+    end
+
+end 
